a5aa6de748e71e6928eba5906c5c856d2b282b92
kkterai <kkterai@gmail.com>
2017-02-23 22:47:04 -0800

    no change

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,11 @@
 class Cart < ActiveRecord::Base
   has_many :items, through: :line_items
   has_many :line_items
+  belongs_to :user
 
 
   def total
-    
+
     ary = self.items.map do |item|
       item.price
     end
@@ -17,18 +18,18 @@ class Cart < ActiveRecord::Base
 
   def add_item(new_item_id)
     @line_item = LineItem.new(item_id: new_item_id)
-    @line_item.cart_id = self.id 
+    @line_item.cart_id = self.id
 
     # @line_item.quantity = 0
     # @line_item.quantity = @line_item.quantity + 1
-    
+
     # #getting error for above two lines:
     #    undefined method `quantity' for 0:Fixnum
     #  # ./spec/models/cart_spec.rb:38:in `block (
 
     # binding.pry
     # @line_item
-    
+
   end
-  
+
 end
