19753ea79662c487cd6602db9ed8ad9379f1f5d7
christinechoi <smi77y@gmail.com>
2017-02-24 13:00:21 -0500

    Add Store#index and basic routes to CartsController

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,19 @@
 class CartsController < ApplicationController
 
+  def index
+    @items = Item.all
+  end
+
+  def show
+    # @item = Item.find
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   
+  def index
+    @categories = Category.all
+    @items = Item.all
+  end
+
+  
+
 end
 
 
