bc80c0da523d8a8b679cb251c4ef3f94fa657203
kkterai <kkterai@gmail.com>
2017-02-24 14:04:02 -0800

    troubleshooting features

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users , skip: [:sessions]
+  as :user do
+    get '/users/sign_in', to: 'devise/sessions#new', as: :new_user_session
+    post '/users/sign_in', to: 'devise/sessions#create', as: :user_session
+    delete '/', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   root 'store#index', as: 'store'
 
   resources :items, only: [:show, :index]
