db9bbb89839c5a94cb99a75e2f9cb9c8e479bc49
christinechoi <smi77y@gmail.com>
2017-02-26 22:49:33 -0500

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,11 +11,15 @@ class CartsController < ApplicationController
   end
 
   def show
-    current_cart = Cart.find_by(id: params[:id])
+    # binding.pry
+    current_cart = current_user.current_cart
+
+    # current_cart = Cart.find_by(id: params[:id])
   end
 
   def checkout
-    current_cart = Cart.find_by(id: params[:id])
+    # binding.pry
+    current_cart = current_user.current_cart#Cart.find_by(id: params[:id])
 
     current_cart.line_items.collect do |line_item|
       @item = Item.find_by(id: line_item.item_id)
@@ -23,10 +27,10 @@ class CartsController < ApplicationController
       @item.save
     end
     current_cart.status = "submitted"
+    current_cart.save
+    current_user.current_cart = nil #somehow current_user.current_cart is not nil
     # binding.pry
-    # current_cart = nil
-    # binding.pry
-    redirect_to cart_path
+    redirect_to cart_path #carts#show
   end
 
   def destroy
