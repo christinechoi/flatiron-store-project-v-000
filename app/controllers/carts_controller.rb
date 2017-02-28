5ebf8391cf567eda4040edce68e318bc3ccaaab6
kkterai <kkterai@gmail.com>
2017-02-27 17:07:50 -0800

    debugging

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,7 +6,7 @@ class CartsController < ApplicationController
 
   def create
     @cart = Cart.create
-    @cart.user_id = current_user.id 
+    @cart.user_id = current_user.id
     redirect_to cart_path(@cart)
   end
 
@@ -18,17 +18,18 @@ class CartsController < ApplicationController
   end
 
   def checkout
-    # binding.pry
-    current_cart = current_user.current_cart#Cart.find_by(id: params[:id])
 
-    current_cart.line_items.collect do |line_item|
+    @cart = current_user.current_cart#Cart.find_by(id: params[:id])
+
+    @cart.line_items.collect do |line_item|
       @item = Item.find_by(id: line_item.item_id)
       @item.inventory = @item.inventory - line_item.quantity
       @item.save
     end
-    current_cart.status = "submitted"
-    current_cart.save
-    current_user.current_cart = nil #somehow current_user.current_cart is not nil
+    @cart.status = "submitted"
+    current_user.save
+    @cart = nil
+    current_user.current_cart = nil#somehow current_user.current_cart is not nil
     # binding.pry
     redirect_to cart_path #carts#show
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def show
+    raise "logging in".inspect
     redirect_to '/'
   end
-  
+
 end
