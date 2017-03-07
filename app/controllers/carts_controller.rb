69ff211e26882c08c1d5144923d637c297ef54ac
kkterai <kkterai@gmail.com>
2017-03-07 14:00:59 -0800

    aligned controller methods to view, styling on items/show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,9 +8,9 @@ class CartsController < ApplicationController
     @cart = current_user.current_cart
     @cart.checkout
     @cart = nil
-    current_user.current_cart_id = nil
+    current_user.current_cart_id = nil #Passing this test requires setting current_cart to nil. However...
     current_user.save
-    redirect_to cart_path
+    redirect_to cart_path #...going to cart_path results in an error.
   end
 
 end
